df0b4cd7696bfd280147c9e3a39498dd92ff84f6
lucaseo <lucaseo0147@gmail.com>
2021-11-30 19:26:25 +0900

    dev branch initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+import mlflow
+import hydra
+from omegaconf import DictConfig
+
+
+
+## Todo
+## add hydra configuration
+## attach wandb
+## attach each step
+def main():
+
+    # Setup the wandb experiment...?
+
+
+    # Get the path at the root of the MLflow project with this:
+
+
+    # Check which steps we need to execute
+
+
+    # Chained pipeline
+
+    # 1. Download step
+
+    # 2. Preprocess step
+
+    # 3. Validation step
+
+    # 4. Segregation step
+
+    # 5. Modelling step
+
+    # 6. Evaluation step
+
+    pass
