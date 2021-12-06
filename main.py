66737aa9f04b63218e01957102265c076a4ad509
lucaseo <lucaseo0147@gmail.com>
2021-12-06 21:09:07 +0900

    update data validation step.

--- main.py
+++ main.py
@@ -63,7 +63,9 @@ def main(config):
             os.path.join(root_path, "validate_data"),
             "main",
             parameters={
-
+                "reference_artifact": config['data']['reference_dataset'],
+                "sample_artifact": "preprocessed_data.csv:latest",
+                "ks_alpha": config['data']['ks_alpha']
             }
         )
 
